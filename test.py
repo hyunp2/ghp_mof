c0973f6ca38451b9c2febc9dd04428914f80f11a
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-06-12 16:52:42 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,12 @@ from data_utils import *
 from dist_utils import *
 from loss_utils import *
 from train_utils import *
+
+if __name__ == "__main__":
+    config = dict(orig_atom_fea_len=92, nbr_fea_len=41,
+         atom_fea_len=64, n_conv=3, h_fea_len=128, n_h=1,
+         classification=False)
+    m = CrystalGraphConvNet(**config)
+    
+    
+    
