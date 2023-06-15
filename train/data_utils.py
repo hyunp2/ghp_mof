adc0beb53ae2a97dfcaf5229477a62d0c1983374
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-06-14 21:28:55 -0500

    Update data_utils.py

--- train/data_utils.py
+++ train/data_utils.py
@@ -43,6 +43,124 @@ import pathlib
 roots = pathlib.Path(__file__).parent.parent
 sys.path.append(roots) #append top directory
 
+__all__ = ["_get_split_sizes", "get_dataloader", "GaussianDistance", "AtomInitializer", 
+           "AtomCustomJSONInitializer", "CIFData", "DataModuleCrystal"]
+
+def _get_split_sizes(train_frac: float, full_dataset: Dataset) -> Tuple[int, int, int]:
+    """DONE: Need to change split schemes!"""
+    len_full = len(full_dataset)
+    len_train = int(len_full * train_frac)
+    len_test = int(0.1 * len_full)
+    len_val = len_full - len_train - len_test
+    return len_train, len_val, len_test  
+  
+def get_dataloader(dataset: Dataset, shuffle: bool, collate_fn: callable=None, **kwargs):
+    sampler = DistributedSampler(dataset, shuffle=shuffle) if dist.is_initialized() else None
+    loader = DataLoader(dataset, shuffle=(shuffle and sampler is None), sampler=sampler, collate_fn=collate_fn, **kwargs)
+    return loader
+
+class GaussianDistance(torch.nn.Module):
+    """
+    Expands the distance by Gaussian basis.
+
+    Unit: angstrom
+    """
+    def __init__(self, dmin, dmax, step, var=None):
+        """
+        Parameters
+        ----------
+
+        dmin: float
+          Minimum interatomic distance
+        dmax: float
+          Maximum interatomic distance
+        step: float
+          Step size for the Gaussian filter
+        """
+        assert dmin < dmax
+        assert dmax - dmin > step
+#         self.filter = np.arange(dmin, dmax+step, step)
+        super().__init__()
+        self.register_buffer("filter", torch.arange(dmin, dmax+step, step).type(torch.float32))
+        if var is None:
+            var = step
+        self.var = var
+
+    def expand(self, distances):
+        """
+        Apply Gaussian disntance filter to a numpy distance array
+
+        Parameters
+        ----------
+
+        distance: np.array shape n-d array
+          A distance matrix of any shape
+
+        Returns
+        -------
+        expanded_distance: shape (n+1)-d array
+          Expanded distance matrix with the last dimension of length
+          len(self.filter)
+        """
+        return torch.exp(-(distances[..., None] - self.filter)**2 /
+                      self.var**2)
+      
+    def forward(self, distances):
+        return self.expand(distances)
+
+
+class AtomInitializer(object):
+    """
+    Base class for intializing the vector representation for atoms.
+
+    !!! Use one AtomInitializer per dataset !!!
+    """
+    def __init__(self, atom_types):
+        self.atom_types = set(atom_types)
+        self._embedding = {}
+
+    def get_atom_fea(self, atom_type):
+        assert atom_type in self.atom_types
+        return self._embedding[atom_type]
+
+    def load_state_dict(self, state_dict):
+        self._embedding = state_dict
+        self.atom_types = set(self._embedding.keys())
+        self._decodedict = {idx: atom_type for atom_type, idx in
+                            self._embedding.items()}
+
+    def state_dict(self):
+        return self._embedding
+
+    def decode(self, idx):
+        if not hasattr(self, '_decodedict'):
+            self._decodedict = {idx: atom_type for atom_type, idx in
+                                self._embedding.items()}
+        return self._decodedict[idx]
+
+
+class AtomCustomJSONInitializer(AtomInitializer):
+    """
+    Initialize atom feature vectors using a JSON file, which is a python
+    dictionary mapping from element number to a list representing the
+    feature vector of the element.
+
+    Parameters
+    ----------
+
+    elem_embedding_file: str
+        The path to the .json file
+    """
+    def __init__(self, elem_embedding_file):
+        with open(elem_embedding_file) as f:
+            elem_embedding = json.load(f)
+        elem_embedding = {int(key): value for key, value
+                          in elem_embedding.items()}
+        atom_types = set(elem_embedding.keys())
+        super(AtomCustomJSONInitializer, self).__init__(atom_types)
+        for key, value in elem_embedding.items():
+            self._embedding[key] = torch.from_numpy(np.array(value)).type(torch.float32)
+
 class CIFData(Dataset):
     """
     The CIFData dataset is a wrapper for a dataset where the crystal structures
