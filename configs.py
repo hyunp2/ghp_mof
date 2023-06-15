90caba802c03813696cca72ca98326fd3db3863d
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-06-15 17:23:45 -0500

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,15 @@
+from models import cgcnn
+import torch
+
+__all__ = ["BACKBONES", "BACKBONE_KWARGS"]
+
+BACKBONES = {
+			"cgcnn": cgcnn.CrystalGraphConvNet
+			}
+
+#WIP for TorchMDNet Configs!
+BACKBONE_KWARGS = {
+			"cgcnn": dict(orig_atom_fea_len=92, nbr_fea_len=41,
+			 atom_fea_len=64, n_conv=3, h_fea_len=128, n_h=1,
+			 classification=False, learnable=False, explain=False)
+			 }
