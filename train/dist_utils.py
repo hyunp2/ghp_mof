2b3e56879a8f1b9f4bb429f2297abfaa8ed144c4
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-06-15 16:55:40 -0500

    Update dist_utils.py

--- train/dist_utils.py
+++ train/dist_utils.py
@@ -45,7 +45,6 @@ from enum import Enum
 from typing import *
 # from wandb.sdk.interface._dtypes import AnyType
 
-from train.molecule_net import from_smiles
 from torch_geometric.data import Batch
 from torch_cluster import radius_graph
 import copy
@@ -208,16 +207,7 @@ class Logger(ABC):
     @rank_zero_only
     def log_explainer(self, model: torch.nn.Module, smiles_list: Union[List[Chem.rdchem.Mol], List[str]], step=None) -> None:
         pass
-        
-#     @rank_zero_only
-#     @abstractmethod
-#     def log_molecules(self, smiles_list: Union[List[Chem.rdchem.Mol], List[str]], step: Optional[int] = None) -> None:
-#         pass
     
-#     @rank_zero_only
-#     @abstractmethod
-#     def log_cams(self, smiles_list: Union[List[Chem.rdchem.Mol], List[str]], step: Optional[int] = None) -> None:
-#         pass
     
 class WandbLogger(Logger):
     def __init__(
@@ -292,71 +282,4 @@ class WandbLogger(Logger):
         artifact = self.experiment.use_artifact(name)
         return artifact.download() #returns directory/file
             
-    @rank_zero_only
-    def log_deephyper(self, dataframe: pd.DataFrame, step: Optional[int] = None) -> None:
-        table = wandb.Table(dataframe=dataframe)
-        if step is not None:
-            self.experiment.log({"DeepHyper Param Search": table})
-        else:
-            self.experiment.log({"DeepHyper Param Search": table})
-    
-    @staticmethod
-    def mol_to_pil_image(smiles_string: str, width: int = 300, height: int = 300) -> "PIL.Image":
-        assert isinstance(smiles_string, str), "input must be a SMILES string..."
-        molecule = Chem.MolFromSmiles(smiles_string) #1 SMILES string
-        Chem.AllChem.Compute2DCoords(molecule)
-        Chem.AllChem.GenerateDepictionMatching2DStructure(molecule, molecule)
-        pil_image = Chem.Draw.MolToImage(molecule, size=(width, height))
-        return pil_image
-    
-    @staticmethod
-    def rdkit_dataframe(smiles_string: str, partial_explainer: functools.partial, batch_index: int, end_idx: int):
-        return {    
-                    "smiles": smiles_string,
-                    # wandb.Molecule from a SMILES string:
-                    "molecule": wandb.Molecule.from_smiles(smiles_string,
-                                                           sanitize = (True),
-                                                            convert_to_3d_and_optimize  = (True),
-                                                            mmff_optimize_molecule_max_iterations = 200),
-                    "molecule_2D": wandb.Image(WandbLogger.mol_to_pil_image(smiles_string)),
-                    "explained": partial_explainer(batch_index=batch_index, smiles=smiles_string, end_idx=end_idx)["figure"]
-               }
-            
-    @rank_zero_only
-    def log_explainer(self, model: torch.nn.Module, smiles_list: Union[List[Chem.rdchem.Mol], List[str]], device=None, radius_cutoff=10., max_num_neighbors=64) -> None:
-        """
-        Must ensure forward/backward prop has the same number of nodes as molecule's atoms!"""
-        data_batch = list(map(from_smiles, smiles_list)) #torch_geometric list of Data instance
-        data_batch = Batch.from_data_list(data_batch) #Batch instance
-        data_batch = data_batch.to(device)
-        batch = data_batch.batch
-
-        model2 = copy.deepcopy(model) #For guided-backprop
-        temp_fwd = model(z=data_batch.z, pos=data_batch.pos, batch=data_batch.batch)[0] #forward flush! and get Energy
-        temp_fwd.backward(gradient=torch.ones_like(temp_fwd)) #backward flush!
-        gb_model = GuidedBackpropReLUModel(model) #Wrap 
-        gb_model(data_batch.z, data_batch.pos, data_batch.batch) #(Forward/Backward done internally)
-        
-        ##Maybe change to unbatch functions?
-        batch_unique, batch_counts = batch.unique(return_counts=True)
-        # batch_counts_cumsum = torch.cat([torch.tensor([0]), batch_counts]).cumsum(dim=0)
-        batch_counts_cumsum = batch_counts.cumsum(dim=0)
-        edge_index = radius_graph(
-            data_batch.pos,
-            r=radius_cutoff,
-            batch=data_batch.batch,
-            loop=True,
-            max_num_neighbors=max_num_neighbors,
-        )
-        partial_explainer = partial(plot_explanations, model=model, gb_model=gb_model, batch=data_batch.batch, edge_index=edge_index) #WIP: edge_index DOES NOT exist!
-        
-        assert len(batch_unique) == len(smiles_list), "num of smiles candidates and num of unique batch IDs must match!"
-        data = list(map(lambda smiles, batch_idx, end_idx: functools.partial(WandbLogger.rdkit_dataframe, partial_explainer=partial_explainer)(batch_index=batch_idx, smiles_string=smiles, end_idx=end_idx),
-                        smiles_list, batch_unique, batch_counts_cumsum )) #list of dict
-
-        dataframe = pd.DataFrame.from_records(data) #dataframe
-        
-        table = wandb.Table(dataframe=dataframe)
-
-        self.experiment.log({"rdkit Mol Explainer Table": table})
             
