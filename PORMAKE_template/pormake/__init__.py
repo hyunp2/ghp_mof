459af20ff49581ed07a902d21ed2aa7b4c60cce3
Ruijie Zhu <ray19970727@gmail.com>
2023-06-22 08:42:46 -0700

    remove PORMAKE folder

--- PORMAKE_template/pormake/__init__.py
+++
@@ -1,6 +0,0 @@
-from .scaler import Scaler
-from .builder import Builder
-from .locator import Locator
-from .topology import Topology
-from .database import Database
-from .building_block import BuildingBlock
