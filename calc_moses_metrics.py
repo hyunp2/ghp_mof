4c195b2435a4a7dd02032d2b271ac36950c5089f
Ruijie Zhu <ray19970727@gmail.com>
2023-06-22 21:43:55 -0700

    update code

--- calc_moses_metrics.py
+++ calc_moses_metrics.py
@@ -3,11 +3,10 @@ import moses
 import pandas as pd
 from tqdm import tqdm
 
-for n_atoms in os.listdir('.'):
-	if 'n_atoms' in n_atoms:
-		for sys in tqdm([i.split('_')[0] for i in os.listdir(os.path.join(n_atoms,'smiles')) if '.txt' in i]):
-			smiles_all = list(pd.read_csv(os.path.join(n_atoms,'info',sys+'_info_sa_sc_filter.csv')).smiles)
+for n_atoms in [i for i in os.listdir('output_for_assembly')if 'n_atoms' in i]:
+		for sys in tqdm([i.split('_')[0] for i in os.listdir(os.path.join('output_for_assembly',n_atoms,'smiles')) if '.txt' in i]):
+			smiles_all = list(pd.read_csv(os.path.join(n_atoms,'info',sys+'_info.csv')).smiles)
 			metrics = moses.get_all_metrics(smiles_all)
-			os.makedirs(os.path.join(n_atoms,'metrics'),exist_ok=True)
-			with open(os.path.join(n_atoms,'metrics',sys+'.txt'),'w+') as f:
+			os.makedirs(os.path.join('output_for_assembly',n_atoms,'metrics'),exist_ok=True)
+			with open(os.path.join('output_for_assembly',n_atoms,'metrics',sys+'.txt'),'w+') as f:
 				f.write(str(metrics))
